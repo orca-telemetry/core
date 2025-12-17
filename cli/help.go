155de4ce64180f843099eed6e04025f0b499a9f9
Frederick Mannings <fred.mannings@solvicode.com>
2025-12-17 22:44:11 +0000

    one step along the way

--- cli/help.go
+++ cli/help.go
@@ -13,6 +13,7 @@ func showHelp() {
 	fmt.Println(infoStyle.Render("  stop    ") + "- Stop the Orca stack")
 	fmt.Println(infoStyle.Render("  status  ") + "- Show status of the Orca components")
 	fmt.Println(infoStyle.Render("  destroy ") + "- Delete all Orca resources")
+	fmt.Println(infoStyle.Render("  stub	") + "- Generate stub types for the local SDK")
 	fmt.Println(
 		infoStyle.Render("  help    ") + "- Show this help message or help for a specific command",
 	)
@@ -22,6 +23,7 @@ func showHelp() {
 	fmt.Println(infoStyle.Render("  orca stop"))
 	fmt.Println(infoStyle.Render("  orca status"))
 	fmt.Println(infoStyle.Render("  orca destroy"))
+	fmt.Println(infoStyle.Render("  orca stub python"))
 	fmt.Println(infoStyle.Render("  orca help start"))
 }
 
@@ -55,6 +57,17 @@ func showCommandHelp(command string) {
 		fmt.Println(infoStyle.Render("\nExample:"))
 		fmt.Println(infoStyle.Render("  orca destroy"))
 
+	case "stub":
+		fmt.Println(subHeaderStyle.Render("'stub' command - Generate Orca stubs"))
+		fmt.Println(infoStyle.Render("\nArguments:"))
+		fmt.Println(infoStyle.Render("\n-out - The directory to print the stubs to (defaults to `./orca-stubs`)"))
+		fmt.Println(infoStyle.Render("\n-connStr - Connection string use when connecting to Orca (defaults to the internal Orca instance)"))
+		fmt.Println(infoStyle.Render("\nUsage:"))
+		fmt.Println(infoStyle.Render("  orca stub <sdk to generate stubs for>"))
+		fmt.Println(infoStyle.Render("\nExamples:"))
+		fmt.Println(infoStyle.Render("  orca stub js"))
+		fmt.Println(infoStyle.Render("  orca stub python"))
+		fmt.Println(infoStyle.Render("  orca stub python -out ./my-orca-project/stubs"))
 	default:
 		fmt.Println(errorStyle.Render(fmt.Sprintf("Unknown command: %s", command)))
 		showHelp()
